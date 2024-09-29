691f635c8773913db009424123b82136a811ef4b
przemek3woo <przemko.wo.fb@gmail.com>
2024-09-29 13:40:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let date = "Oct 05, 2024 12:00:00"
+let emotions = ["🤍", "🥰", "😘", "😻"]
+
+
+let clock = document.getElementById("clock")
+let my_window = document.getElementById("window")
+let enjoy = document.getElementById("enjoy")
+let emo = document.getElementById("emotion")
+
+emo.innerText = emotions[Math.floor(Math.random() * (emotions.length))]
+
+const countDownDate = new Date(date).getTime();
+const interval = setInterval(() => {
+    const now = new Date().getTime();
+    const duration = now - countDownDate;
+    if (duration <= 1) {
+        updateDuration(duration)
+        clearInterval(interval);
+        updateDuration(0);
+        return;
+    }
+    else {
+        getEnjoy()
+    }
+    updateDuration(duration); 
+    }, 1000);
+
+function updateDuration (duration) {
+    let days = Math.floor(duration / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((duration % (1000 * 60)) / 1000);
+    clock.innerText = days + "d, " + hours + "g, " + minutes + "m, " + seconds + "s"
+}
+
+function getEnjoy() {
+    enjoy.style.display = "block"
+    enjoy.style.animation = "none";
+    setTimeout(() => {
+        enjoy.style.transform = "translateY(-100vh)"
+    }, 500)
+    my_window.style.opacity = 1
+    emo.style.opacity = 1
+}
